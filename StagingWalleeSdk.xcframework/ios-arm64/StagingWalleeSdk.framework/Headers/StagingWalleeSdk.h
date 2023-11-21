def20c8b497fc443c147d5b306e18d1ab0bb7443
wallee-deployment-user <technical-systems@wallee.com>
2023-11-21 13:02:44 +0000

    Release version: 1.2.0

---
+++ StagingWalleeSdk.xcframework/ios-arm64/StagingWalleeSdk.framework/Headers/StagingWalleeSdk.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for StagingWalleeSdk.
+FOUNDATION_EXPORT double StagingWalleeSdkVersionNumber;
+
+//! Project version string for StagingWalleeSdk.
+FOUNDATION_EXPORT const unsigned char StagingWalleeSdkVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <StagingWalleeSdk/PublicHeader.h>
